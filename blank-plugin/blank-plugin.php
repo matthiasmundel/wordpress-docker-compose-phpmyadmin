a2c43d8c4dd8639e255e911ea2431f8a43b776fe
Entwicklung <entwicklung@Matthiass-MBP.fritz.box>
2018-10-18 17:10:35 +0200

    Plugin header text

---
+++ blank-plugin/blank-plugin.php
@@ -0,0 +1,12 @@
+/*
+Theme Name: Blank Plugin
+Theme URI: https://localhost:8000/
+Author: Plugin Dev
+Author URI: https://localhost:8000/
+Description: Blank Plugin
+Version: 0.1
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ 
+Blank Plugin.
+*/
