6320010fd07504ee047814ca23e813e2699f5b87
Entwicklung <entwicklung@Matthiass-MBP.fritz.box>
2018-10-18 17:14:22 +0200

    Plugin header text

--- blank-plugin/blank-plugin.php
+++ blank-plugin/blank-plugin.php
@@ -1,12 +1,11 @@
+<?php
 /*
-Theme Name: Blank Plugin
-Theme URI: https://localhost:8000/
-Author: Plugin Dev
-Author URI: https://localhost:8000/
+Plugin Name: Blank Plugin
+Plugin URI: http://localhost:8000/
 Description: Blank Plugin
-Version: 0.1
-License: GNU General Public License v2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
- 
-Blank Plugin.
+Version: 0.0.1
+Author: Author
+Author URI: https://automattic.com/wordpress-plugins/
+License: GPLv2 or later
+Text Domain: blank-plugin
 */
